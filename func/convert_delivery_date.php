2f8a680e4d81ffe599654c94bcb50044f687bf42
sustainablesystems <61712701+sustainablesystems@users.noreply.github.com>
2021-01-27 14:30:19 -0800

    Delete func directory

--- func/convert_delivery_date.php
+++
@@ -1,3 +0,0 @@
-<?php
-$delivery_date = date('j F, Y', strtotime ($delivery_date));
-?>
--- func/javascript_popup.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo '
-  <script type="text/javascript" language="javascript">
-  var new_window = null; function create_window(w,h,url) {
-  var options = "width=" + w + ",height=" + h + ",status=no";
-  new_window = window.open(url, "new_window", options); return false; }
-  </script>';
-?>
